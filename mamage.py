d0ba3a6e2f3ff95dc427235a9af72b8aefa48c51
KimHyunChul <zio4272@naver.com>
2018-08-03 13:06:28 +0900

    #1

--- mamage.py
+++ mamage.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'Hello Webisfree!'
+    return 'Hello World!!'
+
+if __name__ == '__main__':
+  app.run()
